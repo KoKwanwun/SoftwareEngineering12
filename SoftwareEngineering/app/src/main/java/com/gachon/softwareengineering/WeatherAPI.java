616678524af538d9267dde2515fe0d3a625b5f23
wonjongKim <kwjdnjs@gmail.com>
2022-05-23 15:52:38 +0900

    weather api

--- SoftwareEngineering/app/src/main/java/com/gachon/softwareengineering/WeatherAPI.java
+++ SoftwareEngineering/app/src/main/java/com/gachon/softwareengineering/WeatherAPI.java
@@ -1,4 +1,4 @@
-package com.android.clothes_recommendation;
+package com.gachon.softwareengineering;
 
 import android.util.Log;
 
@@ -12,24 +12,63 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class WeatherAPI {
     //API 서비스 키
     private final String SERVICE_KEY = "hXscbGdYsGzEpYjEAL7kZP28p7SfJsLey6PtLZPpAdxZu26rQLSTsAwMkiM3HJh%2BRDEvFSx3P7xgMhJoBwGGzw%3D%3D";
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getRainPerHour() {
+        return rainPerHour;
+    }
+
+    public float getWind() {
+        return wind;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public int getPrecipitation() {
+        return precipitation;
+    }
+
+    //불러온 날씨 데이터
+    private float temperature = 0.0F;
+    private float rainPerHour = 0.0F;
+    private float wind = 0.0F;
+    private int humidity = 0;
+    private int precipitation = 0;
 
     public WeatherAPI(){ }
 
     public void apiLoad() throws IOException {
         //API 요청
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + SERVICE_KEY); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
-        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode("20220522", "UTF-8")); /*‘22년 5월 22일 발표*/
-        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode("0600", "UTF-8")); /*06시 발표(정시단위) */
-        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode("55", "UTF-8")); /*예보지점의 X 좌표값*/
-        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode("127", "UTF-8")); /*예보지점의 Y 좌표값*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "="
+                + SERVICE_KEY); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "="
+                + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "="
+                + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "="
+                + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식 JSON*/
+        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "="
+                + URLEncoder.encode(getDate(), "UTF-8")); /*발표 날짜*/
+        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "="
+                + URLEncoder.encode(getTime(), "UTF-8")); /*정시단위*/
+        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "="
+                + URLEncoder.encode("55", "UTF-8")); /*예보지점의 X 좌표값*/
+        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "="
+                + URLEncoder.encode("127", "UTF-8")); /*예보지점의 Y 좌표값*/
 
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -60,6 +99,8 @@ public class WeatherAPI {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        getTime();
     }
 
     //JSON 파싱
@@ -70,12 +111,40 @@ public class WeatherAPI {
         JSONObject items = body.getJSONObject("items");
         JSONArray itemArray = items.getJSONArray("item");
 
-        Log.d("test", itemArray.get(1).toString());
+        JSONObject item;
+        item = itemArray.getJSONObject(0);
+        precipitation = Integer.parseInt(item.get("obsrValue").toString());
+
+        item = itemArray.getJSONObject(1);
+        humidity = Integer.parseInt(item.get("obsrValue").toString());
+
+        item = itemArray.getJSONObject(2);
+        rainPerHour = Float.parseFloat(item.get("obsrValue").toString());
+
+        item = itemArray.getJSONObject(3);
+        temperature = Float.parseFloat(item.get("obsrValue").toString());
+
+        item = itemArray.getJSONObject(7);
+        wind = Float.parseFloat(item.get("obsrValue").toString());
     }
 
     //날짜 가져오기
-    private String getDate(){}
+    private String getDate(){
+        Date today = new Date();
+        SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
+
+        return date.format(today);
+    }
 
     //시간 가져오기
-    private String getTime(){}
+    private String getTime(){
+        Date today = new Date();
+        TimeZone tz;
+
+        SimpleDateFormat time = new SimpleDateFormat("HH", Locale.KOREAN);
+        tz = TimeZone.getTimeZone("Asia/Singapore");  // TimeZone에 표준시 설정
+        time.setTimeZone(tz);                    //time에 TimeZone 설정
+
+        return time.format(today) + "00";
+    }
 }
